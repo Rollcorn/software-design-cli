ae0f648b62db6e6be1f46611e02c55aaac2e7f44
z1kurat <109900803+z1kurat@users.noreply.github.com>
2024-02-25 23:59:28 +0300

    [z1kurat] fix tab size

--- cli/src/main/java/ru/itmo/streams/StreamImpl.java
+++ cli/src/main/java/ru/itmo/streams/StreamImpl.java
@@ -6,28 +6,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StreamImpl implements Stream {
-    /**
-     * Descriptor of the stream. It is used to identify the stream.
-     * Example: INPUT - for std input stream, ERROR - for std error stream, OUTPUT - for file "output.txt"
-     */
-        private Map<StreamDescriptor, String> data;
+    private Map<StreamDescriptor, String> data;
 
-        public StreamImpl() {
-           this.data = new HashMap<>();
+    public StreamImpl() {
+        this.data = new HashMap<>();
+    }
+
+    public void remove(StreamDescriptor descriptor) {
+        this.data.remove(descriptor);
+    }
+
+    public void put(String data, StreamDescriptor descriptor, Boolean isOverride) {
+        if (isOverride || !this.data.containsKey(descriptor)) {
+            this.data.put(descriptor, data);
         }
+    }
 
-        public void remove(StreamDescriptor descriptor) {
-                this.data.remove(descriptor);
-        }
-
-        public void put(String data, StreamDescriptor descriptor, Boolean isOverride) {
-                if (isOverride || !this.data.containsKey(descriptor)) {
-                        this.data.put(descriptor, data);
-                }
-        }
-
-        public String get(StreamDescriptor descriptor) {
-                return this.data.getOrDefault(descriptor, "");
-        }
-
+    public String get(StreamDescriptor descriptor) {
+        return this.data.getOrDefault(descriptor, "");
+    }
 }
