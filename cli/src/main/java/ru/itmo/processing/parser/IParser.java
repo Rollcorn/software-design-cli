4520540d00ece20cf7d4e3b928bd4cc5d41626a1
AndrewPolyakov1 <mr.dijoy@gmail.com>
2024-02-25 19:41:33 +0300

    Added ISubstitute interface and simple implementation, ParserSimple implementation

--- cli/src/main/java/ru/itmo/processing/parser/IParser.java
+++ cli/src/main/java/ru/itmo/processing/parser/IParser.java
@@ -1,4 +1,10 @@
 package ru.itmo.processing.parser;
 
+import ru.itmo.processing.commands.ICommand;
+
+import java.util.List;
+
 public interface IParser {
+    List<String> tokenizeString(String text);
+    List<ICommand> parse(List<String> tokens);
 }
