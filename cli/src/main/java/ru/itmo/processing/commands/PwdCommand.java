d53247a8b113d3702f31072dc0c7da0ff95149fc
sobark <fedorzharkof@gmail.com>
2024-02-26 11:40:38 +0300

    PwdCommand file

---
+++ cli/src/main/java/ru/itmo/processing/commands/PwdCommand.java
@@ -0,0 +1,12 @@
+package ru.itmo.processing.commands;
+
+import ru.itmo.streams.Stream;
+public class PwdCommand implements ICommand {
+
+    public PwdCommand() {}
+
+    @Override
+    public void execute(Stream stream1) {
+        // TODO: implement (9(((
+    }
+}
