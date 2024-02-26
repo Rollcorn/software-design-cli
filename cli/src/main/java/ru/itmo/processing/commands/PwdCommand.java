cd6912814ffac4adde8bd931fd7f29e9f1b62d0d
sobark <fedorzharkof@gmail.com>
2024-02-26 12:01:09 +0300

    overload Pwd constructor

--- cli/src/main/java/ru/itmo/processing/commands/PwdCommand.java
+++ cli/src/main/java/ru/itmo/processing/commands/PwdCommand.java
@@ -1,9 +1,22 @@
 package ru.itmo.processing.commands;
 
 import ru.itmo.streams.Stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PwdCommand implements ICommand {
 
-    public PwdCommand() {}
+    private List<String> args_;
+    private List<String> flag_;
+    public PwdCommand() {
+        this.args_ = new ArrayList<>();
+        this.flag_ = new ArrayList<>();
+    }
+    public PwdCommand(List<String> args_, List<String> flag_) {
+        this.args_ = args_;
+        this.flag_ = flag_;
+    }
 
     @Override
     public void execute(Stream stream1) {
