fb8fa2df458dcff11d8614a67ed476a15fa44dfb
AndrewPolyakov1 <mr.dijoy@gmail.com>
2024-03-04 01:49:54 +0300

    [Andrew] New test for non-existing variable
    Signed-off-by: AndrewPolyakov1 <mr.dijoy@gmail.com>

--- cli/src/test/java/ru/itmo/processing/parser/SubstituteSimpleTest.java
+++ cli/src/test/java/ru/itmo/processing/parser/SubstituteSimpleTest.java
@@ -17,8 +17,12 @@ class SubstituteSimpleTest {
         context.put("a", "pwd");
         context.put("c", "cat");
 
-        String input = "$a | $c";
-        String result = substitute.substitute(input, context);
-        Assertions.assertEquals(result, "pwd | cat");
+        String input1 = "$a | $c";
+        String result1 = substitute.substitute(input1, context);
+        Assertions.assertEquals(result1, "pwd | cat");
+
+        String input2 = "Empty str >$b";
+        String result2 = substitute.substitute(input2, context);
+        Assertions.assertEquals(result2, "Empty str >");
     }
 }
